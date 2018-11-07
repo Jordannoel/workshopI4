c948e5d9391ac424752af4be4f92262fb3dcc683
Jordan Noel <jordannoel1@live.fr>
2018-11-07 10:59:56 +0100

    Inscription ok

--- src/main/java/com/epsi/blockchainsmokers/workshopi4/controller/LoginController.java
+++ src/main/java/com/epsi/blockchainsmokers/workshopi4/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -29,6 +30,8 @@ public class LoginController {
         this.inscriptionService = inscriptionService;
     }
 
+    /***********************************************INDEX***************************************************/
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
         return PageMapping.INDEX;
@@ -39,13 +42,15 @@ public class LoginController {
         return PageMapping.INDEX;
     }
 
+    /***********************************************INSCRIPTION***************************************************/
+
     @RequestMapping(value = ApplicationUrl.SIGNIN, method = RequestMethod.GET)
     public String inscription() {
         return PageMapping.SIGNIN;
     }
 
     @RequestMapping(value = ApplicationUrl.SIGNIN, method = RequestMethod.POST)
-    public String inscriptionPost(HttpServletRequest req, ModelMap modelMap) {
+    public String inscriptionPost(HttpServletRequest req, RedirectAttributes red) {
         String email = req.getParameter("email");
         String motDePasse = req.getParameter("motDePasse");
         String confirmationMotDePasse = req.getParameter("confirmationMotDePasse");
@@ -56,10 +61,11 @@ public class LoginController {
             HttpSession session = req.getSession();
             session.setAttribute("email", utilisateur.getEmail());
             session.setAttribute("id", utilisateur.getId());
+            red.addFlashAttribute("email", utilisateur.getEmail());
             return REDIRECT + ApplicationUrl.SIGNIN_OK;
         } catch (WorkshopException e) {
-            modelMap.put("errors", e.getMessages());
-            return PageMapping.SIGNIN;
+            red.addFlashAttribute("errors", e.getMessages());
+            return REDIRECT + ApplicationUrl.SIGNIN;
         }
     }
 
@@ -69,6 +75,8 @@ public class LoginController {
         modelMap.put("email", session.getAttribute("email"));
         return PageMapping.SIGNIN_OK;
     }
+
+    /***********************************************CONNEXION***************************************************/
 
     @RequestMapping(value = ApplicationUrl.LOGIN, method = RequestMethod.GET)
     public String connexion() {
@@ -90,6 +98,8 @@ public class LoginController {
             return PageMapping.LOGIN;
         }
     }
+
+    /***********************************************DECONNEXION***************************************************/
 
     @RequestMapping(value = ApplicationUrl.LOGOUT, method = RequestMethod.POST)
     public String deconnexionPost(HttpServletRequest request) {
--- src/main/java/com/epsi/blockchainsmokers/workshopi4/model/Utilisateur.java
+++ src/main/java/com/epsi/blockchainsmokers/workshopi4/model/Utilisateur.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 public class Utilisateur {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
@@ -23,5 +24,8 @@ public class Utilisateur {
     public Utilisateur(String email, String password) {
         this.email = email;
         this.password = password;
+    }
+
+    public Utilisateur() {
     }
 }
